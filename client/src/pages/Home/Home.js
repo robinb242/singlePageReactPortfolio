b59f81f5341348d9db2d89763505fd745220fbfd
robinb242 <robin.bertuccelli@gmail.com>
2019-12-05 15:44:27 -0600

    routine update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Container, Segment, Image } from 'semantic-ui-react';
 import "./Home.css";
 import Portfolio from '../Portfolio/Portfolio';
+import TechnologiesIcons from '../../components/TechnologiesIcons/TechnologiesIcons';
 import Resume from '../Resume/Resume';
 import SlickDev from '../../components/SlickDev/SlickDev';
 import Contact from '../Contact/Contact';
@@ -38,70 +39,9 @@ class Home extends React.Component {
      machines.
     </h3> 
   </Segment>
-{/* Table of technologies */}
-  <Segment>
 
-    <Grid columns={6}>
-    
-      <Grid.Row>
-        <Grid.Column id="HTML5_icon">
-           <a href="https://www.w3.org/TR/html52/"  rel="noreferrer noopener" target="_blank" className="nav-link"> <Image src={require('../../assets/images/icons/html5.png')}  alt="HTML5 Logo" size='tiny' /> </a> 
-        </Grid.Column>
-
-        <Grid.Column id="CSS3_icon"> 
-          <a href="https://www.w3.org/Style/CSS/" rel="noreferrer noopener" target="_blank" className="nav-link"> <Image src={require('../../assets/images/icons/css3.png')}  alt="CSS3 Logo" size='tiny' /> </a> 
-        </Grid.Column>
-
-        <Grid.Column id="javascript_icon">
-          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" rel="noreferrer noopener" target="_blank" className="nav-link"> <Image src={require('../../assets/images/icons/javascript.png')}  alt="Javascript Logo" size='tiny' /> </a> 
-        </Grid.Column>
-      
-        <Grid.Column id="jquery_icon">
-        <a href="https://jquery.com/" rel="noreferrer noopener" target="_blank" className="nav-link"> <Image src={require('../../assets/images/icons/jquery.png')}  alt="JQuery Logo" size='tiny' /> </a> 
-        </Grid.Column>
-
-        <Grid.Column id="mysql_icon">
-        <a href="https://dev.mysql.com/doc/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/mysql.png')} alt="MySQL Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-        <Grid.Column id="MongoDB_icon">
-        <a href="https://www.mongodb.com/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/mongo.jpg')} alt="MongoDB Logo" size='tiny' /> </a>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column id="nodeJS_icon"> 
-        <a href="https://nodejs.org" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/node_js.png')} alt="NodeJS Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-        <Grid.Column id="rreactJS_icon">
-        <a href="https://reactjs.org/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/react.png')} alt="React Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-        <Grid.Column id="drupal_icon">
-        <a href="http://www.drupal.com/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/drupal.png')} alt="Drupal Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-        <Grid.Column id="expressjs_icon">
-        <a href="https://expressjs.com/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/expressjs.png')} alt="Express Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-        <Grid.Column id="handlebars_icon">
-        <a href="https://handlebarsjs.com/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/handlebars.png')} alt="Express Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-        <Grid.Column id="semanticUI_icon">
-        <a href="https://react.semantic-ui.com/" rel="noreferrer noopener" target="blank" className="nav-link"> <Image src={require('../../assets/images/icons/semanticUI.png')} alt="Express Logo" size='tiny' /> </a>
-        </Grid.Column>
-
-
-      </Grid.Row>
-    
-    </Grid>
-  </Segment>
-
- 
       <Portfolio />
+      <TechnologiesIcons />
 
       {/*}
       <div>
